8cb14a7088e2dbb90df88e6eebee23fa2add6c7d
cuixiaorui <cui_xiaorui@126.com>
2021-07-30 22:27:38 +0800

    feat: room history

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     hello
-    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <div>
+      <RootHistory></RootHistory>
+    </div>
   </div>
 </template>
 
 <script setup>
-import axios from "axios";
-
-const { data } = await axios.get("/api/room_history");
-
-console.log(data);
-
+import RootHistory from "./components/RootHistory.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
---
+++ view/src/components/RootHistory.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in messages" :key="index">{{ item.text }}</li>
+  </ul>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const messages = ref([]);
+
+// 1秒请求一次
+setInterval(async () => {
+  const { data } = await axios.get("/api/room_history");
+  messages.value.push(...data.data);
+}, 1000);
+</script>
+
+<style></style>
